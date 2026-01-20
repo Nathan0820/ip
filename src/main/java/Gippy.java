5ed1ae01fcf684c9d3e6c9e4598d87edd18cc402
Nathan0820 <nathanwyj0629@gmail.com>
2026-01-20 23:07:58 +0800

    Add echo

--- src/main/java/Gippy.java
+++ src/main/java/Gippy.java
@@ -1,11 +1,32 @@
+import java.util.Scanner;
+
 public class Gippy {
     public static void main(String[] args) {
-        String line = "____________________________________________________________";
+        //initialize scanner and horizontal line
+        Scanner scanner = new Scanner(System.in);
+        String line = "    ____________________________________________________________";
+        //greet
         System.out.println(line);
-        System.out.println(" Hello! I'm Gippy");
-        System.out.println(" What can I do for you?");
+        System.out.println("     Hello! I'm Gippy");
+        System.out.println("     What can I do for you?");
         System.out.println(line);
-        System.out.println(" Bye. Hope to see you again soon!");
+
+        //echo
+        while (true){
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            else {
+                System.out.println(line);
+                System.out.println("    " + input);
+                System.out.println(line);
+            }
+        }
+
+        //exit
+        System.out.println(line);
+        System.out.println("     Bye. Hope to see you again soon!");
         System.out.println(line);
     }
 }
