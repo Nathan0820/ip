51ed0ebe00fa2e2ee92abaeccb821cf3752fd4f7
Nathan0820 <nathanwyj0629@gmail.com>
2026-01-20 23:22:17 +0800

    Add list

--- src/main/java/Gippy.java
+++ src/main/java/Gippy.java
@@ -1,25 +1,33 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Gippy {
     public static void main(String[] args) {
-        //initialize scanner and horizontal line
+        //initialize variables
         Scanner scanner = new Scanner(System.in);
         String line = "    ____________________________________________________________";
+        ArrayList<String> tasks = new ArrayList<>();
         //greet
         System.out.println(line);
         System.out.println("     Hello! I'm Gippy");
         System.out.println("     What can I do for you?");
         System.out.println(line);
 
-        //echo
+        //store task in arraylist and list them out
         while (true){
             String input = scanner.nextLine();
             if (input.equals("bye")) {
                 break;
-            }
-            else {
+            } else if (input.equals("list")) {
                 System.out.println(line);
-                System.out.println("    " + input);
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println("    " + (i + 1) + ". " + tasks.get(i));
+                }
+                System.out.println(line);
+            } else {
+                tasks.add(input);
+                System.out.println(line);
+                System.out.println("    added: " + input);
                 System.out.println(line);
             }
         }
